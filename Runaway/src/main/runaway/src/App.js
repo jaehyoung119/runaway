5c50907cee57cfba3878b3bd68b6a5774307071d
재형 <140633755+jaehyoung119@users.noreply.github.com>
2024-01-26 10:56:25 +0900

    하하

---
+++ Runaway/src/main/runaway/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import mainImg from './image/다운로드.jpg';
+
+const App = () => {
+  return (
+    <div>
+      <div className='App'>
+        <img alt='' src={mainImg}/>
+        <h2>배포성공하기</h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
